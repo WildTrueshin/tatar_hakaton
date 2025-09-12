e0f619baa723a80d530587247584693c7baadd5c
Wild <wildtrueshin@gmail.com>
2025-09-12 22:46:29 +0300

    add data_helper with load_game(), save_game(scene_name)

---
+++ data_helper.py
@@ -0,0 +1,10 @@
+import json
+
+def load_game():
+    with open("data.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
+
+def save_game(scene_name):
+    with open("data.json", "w", encoding="utf-8") as f:
+        json.dump(scene_name, f, indent=4)
