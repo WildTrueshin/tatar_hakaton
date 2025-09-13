f5f9c1d023a5283de67b0679a5c85b71a4245e7d
Wild <wildtrueshin@gmail.com>
2025-09-13 10:24:20 +0300

    add hint sprite add hint support in render

--- render.py
+++ render.py
@@ -17,6 +17,11 @@ DIALOG_HEIGHT = int(HEIGHT / 5)
 DIALOG_FONT = pygame.font.SysFont("consolas", int(HEIGHT / 20))
 BORDER_COLOR = (117, 56, 56)
 BORDER_WIDTH = int(HEIGHT / 200)
+E_SIZE = int(HEIGHT / 10)
+E_PATH = "sprites/system/use_e.png"
+E_SPRITE = pygame.image.load(E_PATH)
+E_SPRITE = pygame.transform.scale(E_SPRITE, (E_SIZE, E_SIZE))
+E_RECT = pygame.Rect(0, 0, E_SIZE, E_SIZE)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.DOUBLEBUF)
 pygame.display.set_caption("Checheck game")
@@ -47,20 +52,20 @@ while running:
     sorted_objects.append(scene_info["player"])
     sorted_objects.sort(key=lambda obj: obj["z"])
 
-    for object in sorted_objects:
-        rect = object["rect"]
+    for obj in sorted_objects:
+        rect = obj["rect"]
         x_l = rect.x1 * SCALE
         y_t = rect.y1 * SCALE
         width = (rect.x2 - rect.x1) * SCALE
         height = (rect.y2 - rect.y1) * SCALE
 
-        sprite = pygame.image.load(object["texture_path"]).convert_alpha()
+        sprite = pygame.image.load(obj["texture_path"]).convert_alpha()
         sprite = pygame.transform.scale(sprite, (width, height))
         rect = sprite.get_rect(topleft=(x_l, y_t))
         screen.blit(sprite, rect)
 
     if scene_info["ui"]["mode"] == "hint":
-        pass
+        screen.blit(E_SPRITE, E_RECT)
     if scene_info["ui"]["mode"] == "dialog":
         dialog_rect = pygame.Rect(0, HEIGHT - DIALOG_HEIGHT, WIDTH, DIALOG_HEIGHT)
         pygame.draw.rect(screen, DIALOG_COLOR, dialog_rect, border_radius=20)
@@ -68,7 +73,7 @@ while running:
         text_surface = DIALOG_FONT.render(scene_info["ui"]["text"], True, TEXT_COLOR)
         text_rect = text_surface.get_rect(center=dialog_rect.center)
         screen.blit(text_surface, text_rect)
-    else:
+    elif scene_info["ui"]["mode"] == "hidden":
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w] or keys[pygame.K_UP]:
             current_scene.move_forward()
